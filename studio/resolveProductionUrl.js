964b7c0719c7fed2dcf84adc4ac5fbbf981a61b3
dcdev67 <dan.campbell@vercel.com>
2023-01-31 10:55:13 -0600

    adjust sanity preview url to account for prd

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,6 +1,18 @@
-const previewSecret = "ZZZ"; // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = "http://localhost:3000";
+let productionUrl;
+try {
+  productionUrl = new URL(
+    process.env.SANITY_STUDIO_PREVIEW_URL || "http://localhost:3000"
+  );
+} catch (err) {
+  console.error("Invalid productionUrl", err);
+}
 
 export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`;
+  if (!productionUrl || !document.slug?.current) {
+    return false;
+  }
+  const searchParams = new URLSearchParams();
+  searchParams.set("secret", process.env.SANITY_STUDIO_PREVIEW_SECRET || "");
+  searchParams.set("slug", document.slug.current);
+  return `${productionUrl.origin}/api/preview?${searchParams}`;
 }
